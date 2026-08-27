7b18aa8f2ec16d8cfdf33253ffc72af0626a81c6
navin-s09 <navin.s@fulcrumdigital.com>
2026-08-27 21:55:26 +0530

    feature: add SQLAlchemy session and base

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -11,3 +12,12 @@ if not DATABASE_URL:
 engine = create_engine(
     DATABASE_URL,
 )
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+class Base(DeclarativeBase):
+    pass
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,21 @@
 from sqlalchemy import text
-from app.db.database import engine
+
+from app.db.database import Base, SessionLocal, engine
+
+
 def test_database_connection():
     with engine.connect() as connection:
         result = connection.execute(text("SELECT 1"))
 
         assert result.scalar() == 1
+def test_database_session():
+    db = SessionLocal()
+
+    try:
+        result = db.execute(text("SELECT 1"))
+
+        assert result.scalar() == 1
+    finally:
+        db.close()
+def test_base_exists():
+    assert  Base is not None
